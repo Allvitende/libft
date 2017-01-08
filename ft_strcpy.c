721cdb2fb66650578a39e025bfa9f41138368ff7
Brodie Schroeder <bschroed@e1z1r7p3.7.111.10.in-addr.arpa>
2017-01-07 16:15:10 -0800

    Modifying ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -14,16 +14,8 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	int i;
+	char *ret = dst;
 
-	if (!dst || !src)
-		return (0);
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
+	while(*dst++ = *src++);
+	return (ret);
 }
