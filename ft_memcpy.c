e1d7e7ea03fc0effd03ce6f86089d42c7e1f65dc
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-13 19:48:16 -0800

    Add ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschroed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/13 17:19:57 by bschroed          #+#    #+#             */
+/*   Updated: 2017/01/13 17:19:58 by bschroed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	while (n--)
+		((unsigned char *)dst)[n] = ((unsigned char *)src)[n];
+	return (dst);
+}
