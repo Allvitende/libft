ce3ff03f65d5565fe0bca06c38310022ed8ae5e4
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-13 16:33:45 -0800

    Fix libft.h to include ft_memset.c

--- libft.h
+++ libft.h
@@ -18,7 +18,7 @@
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
 void	ft_putstr(char const *s);
-void	*memset(void *b, int c, size_t len);
+void	ft_memset(void *b, int c, size_t len);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
