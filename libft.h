145512edc5e6611c53dc807098087214415c567a
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-13 16:11:16 -0800

    Update libft.h

--- libft.h
+++ libft.h
@@ -18,17 +18,26 @@
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
 void	ft_putstr(char const *s);
+void	*memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len_a);
+char	*ft_strstr(const char *big, const char *little);
 size_t	ft_strlen(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
 int		ft_isupper(int c);
 int		ft_islower(int c);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isspace(int c);
 int		ft_isalnum(int c);
+int		ft_atoi(const char *str);
 
 #endif
