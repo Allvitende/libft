199251732f202ebae2cc180fb4ebf402f2cf62f9
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-15 03:31:46 -0800

    Add ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschroed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/15 03:17:01 by bschroed          #+#    #+#             */
+/*   Updated: 2017/01/15 03:17:02 by bschroed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	while (n--)
+	{
+		if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
+			return (*(unsigned char *)(s1 - 1) - *(unsigned char *)(s2 - 1));
+	}
+	return (0);
+}
