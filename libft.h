8277184df8cf85841b8b91abb3cf1b311629f13a
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-26 02:39:28 -0800

    Add ft_strmapi prototype

--- libft.h
+++ libft.h
@@ -44,6 +44,7 @@ char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strmap(const char *s, char (*f)(char));
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_strcmp(const char *s1, const char *s2);
