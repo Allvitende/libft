912f721da72db9961699fc2d881a9af40bbd993e
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-26 02:39:13 -0800

    Add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschroed <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/26 02:33:25 by bschroed          #+#    #+#             */
+/*   Updated: 2017/01/26 02:33:26 by bschroed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char *dst;
+	unsigned int i;
+	size_t size;
+
+	size = ft_strlen(s) + 1;
+	i = 0;
+	dst = (char *)malloc(sizeof(char) * size);
+	if (!dst)
+		return (NULL);
+	while (s[i])
+	{
+		*dst++ = f(i, s[i]);
+		i++;
+	}
+	*dst = '\0';
+	return (dst - (size - 1));
+}
