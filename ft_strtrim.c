067ba56f8e56e00f56014f8d8acefb13e81f3b22
Brodie Schroeder <bschroed@student.42.us.org>
2017-01-31 02:34:32 -0800

    Refactor for norminette

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,44 @@
 /*   By: bschroed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 23:02:58 by bschroed          #+#    #+#             */
-/*   Updated: 2017/01/31 01:14:21 by bschroed         ###   ########.fr       */
+/*   Updated: 2017/01/31 02:33:43 by bschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static	size_t	trimfront(char const *s)
 {
 	size_t i;
-	size_t j;
-	size_t k;
-	char *ret;
 
 	i = 0;
-	k = 0;
-	if (s[k] == '\0')
-		return ("\0");
 	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-	{
 		i++;
-		if (s[i] == '\0')
-			return ("\0");
-	}
-	j  = ft_strlen(s) - 1;
+	return (i);
+}
+
+static	size_t	trimback(char const *s)
+{
+	size_t j;
+
+	j = ft_strlen(s) - 1;
 	while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
 		j--;
+	return (j);
+}
+
+char			*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	*ret;
+
+	i = trimfront(s);
+	j = trimback(s);
+	k = 0;
+	if (s[i] == '\0')
+		return ("\0");
 	ret = (char *)malloc(sizeof(char) * ((j - i) + 2));
 	if (!ret)
 		return (NULL);
